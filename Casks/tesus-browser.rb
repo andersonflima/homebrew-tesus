92de7baa23da337969459f8ee15e031ea4ff7cd3
Anderson Faustino Lima <andersonfaustinolima@gmail.com>
2026-07-21 14:24:04 -0300

    tesus-browser 0.1.15

--- Casks/tesus-browser.rb
+++ Casks/tesus-browser.rb
@@ -1,6 +1,6 @@
 cask "tesus-browser" do
-  version "0.1.14"
-  sha256 "1ca521179dcb6c076234de06e5063d1a65a731705d92c36ad1be5e227fdb19d4"
+  version "0.1.15"
+  sha256 "c9b0de373f3b85283d756b8ffe8c9b12b69a42da44dc37a3b9a586abc3d98578"
 
   # Servido pelo release do GitHub (CDN rápido). Espelho: https://tesus.com.br/dist/Tesus-Browser-#{version}-arm64.dmg
   url "https://github.com/andersonflima/homebrew-tesus/releases/download/v#{version}/Tesus-Browser-#{version}-arm64.dmg"
