5c335ca9c2e74daa50cc381c2e45cda0f48ad326
Tesus Release <release@tesus.com.br>
2026-08-03 09:13:55 +0000

    chore: publica Tesus Desktop 0.1.2 no cask

--- Casks/tesus-desktop.rb
+++ Casks/tesus-desktop.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 cask "tesus-desktop" do
-  version "0.1.1"
+  version "0.1.2"
 
   on_arm do
-    sha256 "4997fb5472c4f664ef4c2ab26b531850d2367604a74e76e6a63158f73190c1a2"
+    sha256 "cc7ac6d56c5455d4fbec31bc3788a7f669861e25926cb86e92034a9876bf4591"
     url "https://github.com/andersonflima/homebrew-tesus/releases/download/desktop-v#{version}/Tesus-Desktop-#{version}-arm64.dmg",
         verified: "github.com/andersonflima/homebrew-tesus/"
   end
 
   on_intel do
-    sha256 "93fd61a4a122502c1d4090563fb3a65f0f8ff2206cb7016ea19a8d77b2fefb26"
+    sha256 "8bda3b7a580f8fae41393441c4fafda18ffdcb179aa2d55ae4355ae425227336"
     url "https://github.com/andersonflima/homebrew-tesus/releases/download/desktop-v#{version}/Tesus-Desktop-#{version}-x64.dmg",
         verified: "github.com/andersonflima/homebrew-tesus/"
   end
