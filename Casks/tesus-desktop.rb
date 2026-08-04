75bf5679b8f54c95611215a1e5ea0e99c9b3a97b
Anderson Espindola <54826898+andersonflima@users.noreply.github.com>
2026-08-04 06:48:14 -0300

    chore: publica Tesus Desktop 0.1.8 no cask (#9)
    Co-authored-by: Tesus Release <release@tesus.com.br>

--- Casks/tesus-desktop.rb
+++ Casks/tesus-desktop.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
 cask "tesus-desktop" do
-  version "0.1.7"
+  version "0.1.8"
 
   on_arm do
-    sha256 "213f88c96e84c3cee61c6cdde64ccd040b0192fd7e2d34dc2d639634b4b54d06"
+    sha256 "d7601840112f237e80bb045d5e0faf800c49f5763561890308c9517038c285bb"
     url "https://github.com/andersonflima/homebrew-tesus/releases/download/desktop-v#{version}/Tesus-Desktop-#{version}-arm64.dmg",
         verified: "github.com/andersonflima/homebrew-tesus/"
   end
 
   on_intel do
-    sha256 "19397468d2bef976dd8d7d603f38692d7907f146a6534c83f5adc35cfce4375e"
+    sha256 "d170a764a4962c1722ac880a191890a8b180e28ab6f98a115f34f21c5176a13c"
     url "https://github.com/andersonflima/homebrew-tesus/releases/download/desktop-v#{version}/Tesus-Desktop-#{version}-x64.dmg",
         verified: "github.com/andersonflima/homebrew-tesus/"
   end
